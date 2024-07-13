bec816b4ff19d56673a5d065a6288caf88009dfc
nowgnas <dev.nowgnas@gmail.com>
2024-07-13 16:20:14 +0900

    initial commit

---
+++ discovery-service/src/test/java/msa/adaptive/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.adaptive.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
